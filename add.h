24bab19f668502b13efc006ea609525e7741cf1a
chinmaya <chinmaya.sai161@gmail.com>
2021-05-14 19:45:47 +0530

    initial commit

---
+++ add.h
@@ -0,0 +1,6 @@
+#ifndef ADD_2
+#define ADD_2
+
+int add_2(int, int);
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<add.h>
+
+using namespace std;
+
+int main()
+{
+ //commenting
+ cout<<add_2(10,15)<<endl; 
+ return 0; 
+}
